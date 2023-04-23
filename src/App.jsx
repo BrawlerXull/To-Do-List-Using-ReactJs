a76a31b27ccf7314500de1ab7831c576a1431da1
Chinmay Chaudhari <chinmay@Chinmays-MacBook-Air.local>
2023-04-24 03:31:41 +0530

    Added deleting individual todo functionality

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,12 @@ function App() {
   const [todos, setTodos] = useState([]);
 
   var text = useRef();
+
+  function deleteIt(index){
+    const newTodos = todos.filter((todo, i) => i !== index);
+    setTodos(newTodos);
+  }
+
   function addTodo() {
     event.preventDefault();
     var x = text.current.value;
@@ -26,7 +32,10 @@ function App() {
     <div>
       <ul>
         {todos.map((todo, index) => (
-          <li key={index}> {todo}</li>
+          <li key={index}> <div className="todo">
+            <h4>{todo}</h4>
+            <button onClick={()=>deleteIt(index)}>Delete</button>
+            </div></li>
         ))}
       </ul>
       <input type="text" placeholder="Enter the todo here" ref={text} />
